bc60b4d2d88d92e5d53af1e7a95477b012997e11
AbdullahDaro <abode.morx@gmail.com>
2023-10-02 19:13:27 +0300

    ADD

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='firstLine-footer'>
+        <p>About us </p>
+        <p>Type of transfer</p>
+        <p>Our Blog</p>
+        <p>Things to do in Turkey</p>
+        <p>Tours & Activities</p>
+        <p>Become a partner</p>
+      </div>
+      <div className='secondLine-footer'>
+        <p>Contact us </p>
+        <p>Terms & Conditions</p>
+        <p>Privacy Policy</p>
+        <p>FAQ</p>
+      </div>  
+    </div>
   )
 }
 
